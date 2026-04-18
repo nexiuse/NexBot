5f2516d9082e6add5f7d71e1f7b695e43705c0f1
nexiuse <yogawahyu0308@gmail.com>
2026-04-18 20:35:43 +0700

    Delete deobfuscator/__init__.py

--- deobfuscator/__init__.py
+++
@@ -1,6 +0,0 @@
-from .engine import DeobfuscationEngine
-from .detector import detect_obfuscator
-from .unwrapper import unwrap_layers
-from .beautifier import beautify_lua
-
-__all__ = ["DeobfuscationEngine", "detect_obfuscator", "unwrap_layers", "beautify_lua"]
